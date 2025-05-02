743e5d295f30e690e87b4a96802bf167334132c4
jeyakumar <balaji.jeyakumar@mara.com>
2025-05-02 16:15:15 +0530

    updating changes for chroma

--- contrib/test.sh
+++ contrib/test.sh
@@ -1,70 +1,52 @@
-#!/bin/sh -ex
+#!/bin/sh
 
-FEATURES="base64 bitcoinconsensus use-serde rand"
+set -ex
 
-pin_common_verions() {
-    cargo generate-lockfile --verbose
-    cargo update -p cc --precise "1.0.41" --verbose
-    cargo update -p serde --precise "1.0.98" --verbose
-    cargo update -p serde_derive --precise "1.0.98" --verbose
-}
+CRATES="bitcoin hashes internals fuzz"
+DEPS="recent minimal"
+MSRV="1\.48\.0"
 
-# Pin `cc` for Rust 1.29
-if [ -n "$PIN_VERSIONS" ]; then
-    pin_common_verions
-    cargo update -p byteorder --precise "1.3.4"
+# Test pinned versions.
+if cargo --version | grep ${MSRV}; then
+    cargo update -p serde_json --precise 1.0.99
+    cargo update -p serde --precise 1.0.156
+    cargo update -p quote --precise 1.0.30
+    cargo update -p proc-macro2 --precise 1.0.63
+    cargo update -p serde_test --precise 1.0.175
+    # Have to pin this so we can pin `schemars_derive`
+    cargo update -p schemars --precise 0.8.12
+    # schemars_derive 0.8.13 uses edition 2021
+    cargo update -p schemars_derive --precise 0.8.12
+    # memchr 2.6.0 uses edition 2021
+    cargo update -p memchr --precise 2.5.0
+    # byteorder 1.5.0 uses edition 2021
+    cargo update -p byteorder --precise 1.4.3
+
+    # Build MSRV with pinned versions.
+    cargo check --all-features --all-targets
 fi
 
-if [ "$DO_COV" = true ]
-then
-    export RUSTFLAGS="-C link-dead-code"
-fi
-
-
-# Use toolchain if explicitly specified
-if [ -n "$TOOLCHAIN" ]
-then
-    alias cargo="cargo +$TOOLCHAIN"
-fi
-
-# Test without any features first
-cargo test --verbose --no-default-features
-# Then test with the default features
-cargo test --verbose
-
-# Test each feature
-for feature in ${FEATURES}
+for dep in $DEPS
 do
-    cargo test --verbose --features="$feature"
+    cp "Cargo-$dep.lock" Cargo.lock
+    for crate in ${CRATES}
+    do
+        (
+            cd "$crate"
+            ./contrib/test.sh
+        )
+    done
+    if [ "$dep" = recent ];
+    then
+        # We always test committed dependencies but we want to warn if they could've been updated
+        cargo update
+        if diff Cargo-recent.lock Cargo.lock;
+        then
+            echo Dependencies are up to date
+        else
+            echo "::warning file=Cargo-recent.lock::Dependencies could be updated"
+        fi
+    fi
 done
 
-# Fuzz if told to
-if [ "$DO_FUZZ" = true ]
-then
-    (
-        cd fuzz
-        cargo test --verbose
-        ./travis-fuzz.sh
-    )
-fi
-
-# Bench if told to
-if [ "$DO_BENCH" = true ]
-then
-    cargo bench --features unstable
-fi
-
-# Use as dependency if told to
-if [ -n "$AS_DEPENDENCY" ]
-then
-    cargo new dep_test
-    cd dep_test
-    echo 'bitcoin = { path = "..", features = ["use-serde"] }' >> Cargo.toml
-
-    # Pin `cc` for Rust 1.29
-    if [ -n "$PIN_VERSIONS" ]; then
-        pin_common_verions
-    fi
-
-    cargo test --verbose
-fi
+exit 0
